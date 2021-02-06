b4dfae78e7dad10eb3146821dd968c6690847ec9
Jimin947 <jimin947@gmail.com>
2021-02-06 09:34:23 +0900

    Update main.js
    Test Input

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,18 @@
 // main.js
 
-// TODO
+//Input Field
+
+document.getElementById('volume-number').addEventListener("input", function(){
+  
+  var val = document.getElementById("volume-slider").value 
+  
+  document.getElementByID("volume-number").value = val
+
+//Slider
+
+//Volume Icon
+
+//Radio
+
+//Button
+
